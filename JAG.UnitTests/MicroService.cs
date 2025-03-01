7baf9c3fed3548bebc178e17dfbd02a2d750372c
Jim Lightfoot <jim.lightfoot@outlook.com>
2025-03-01 10:52:13 -0800

    add aditional files for JAG

---
+++ JAG.UnitTests/MicroService.cs
@@ -0,0 +1,107 @@
+﻿using JTran.Extensions;
+using System.Reflection;
+using System.Xml.Linq;
+
+namespace JAG.UnitTests
+{
+    [TestClass]
+    public sealed class MicroServiceTests
+    {
+        [TestMethod]
+        [DataRow("rota-inventory", "dev")]
+        public void MicroService_run(string name, string environment)
+        {
+            // First create the JAG file. JAG is a Json schema with a manifest of Azure resources to deploy
+            var jag = CreateJAGFile(name, environment);
+
+            Assert.IsNotNull(jag);
+
+            var flat = CreateFlatManifest(jag);
+
+            Assert.IsNotNull(flat);
+
+            var arm = CreateARMTemplate(flat);
+
+            Assert.IsNotNull(arm);
+
+            File.WriteAllText("c:\\Documents\\Testing\\JAG\\" + name + ".jag", jag);
+            File.WriteAllText("c:\\Documents\\Testing\\JAG\\" + name + ".flat", flat);
+            File.WriteAllText("c:\\Documents\\Testing\\JAG\\" + name + ".arm", arm);
+        }
+
+        private string CreateJAGFile(string name, string environment)
+        {
+            var template = LoadTemplate(name + ".jtran");
+            var include = LoadTemplate("microservice.jtran");
+            var result  = JTran.TransformerBuilder
+                               .FromString(template)
+                               .AddInclude("microservice.jtran", include)
+                               .AddArguments(new Dictionary<string, object> { {"environment", environment} } )
+                               .Build<string>()
+                               .Transform("{ 'dummy': null}");
+
+            return result;
+        }
+
+        private string CreateFlatManifest(string jagFile)
+        {
+            var template = LoadTemplate("jagtoflat.jtran");
+            var result   = JTran.TransformerBuilder
+                                .FromString(template)
+                                .AddInclude("resourcename",      LoadInclude("resourcename.jtran"))
+                                .AddInclude("storage",           LoadResourceInclude("storageaccount.jtran"))
+                                .AddInclude("appinsights",       LoadResourceInclude("applicationinsights.jtran"))
+                                .AddInclude("keyvault",          LoadResourceInclude("keyvault.jtran"))
+                                .AddInclude("appserviceplan",    LoadResourceInclude("appserviceplan.jtran"))
+                                .AddInclude("functionapp",       LoadResourceInclude("functionapp.jtran"))
+                                .Build<string>()
+                                .Transform(jagFile);
+
+            return result;
+        }
+
+        private string CreateARMTemplate(string jagFile)
+        {
+            var template = LoadTemplate("jagtoarm.jtran");
+            var result   = JTran.TransformerBuilder
+                                .FromString(template)
+                                .AddInclude("resourcename",      LoadInclude("resourcename.jtran"))
+                                .AddInclude("storage",           LoadResourceInclude("storageaccount.jtran"))
+                                .AddInclude("appinsights",       LoadResourceInclude("applicationinsights.jtran"))
+                                .AddInclude("keyvault",          LoadResourceInclude("keyvault.jtran"))
+                                .AddInclude("appserviceplan",    LoadResourceInclude("appserviceplan.jtran"))
+                                .AddInclude("functionapp",       LoadResourceInclude("functionapp.jtran"))
+                                .Build<string>()
+                                .Transform(jagFile);
+
+            return result;
+        }
+
+        private string LoadInclude(string fileName)
+        {
+            var path = Path.Combine(Assembly.GetExecutingAssembly().Location.SubstringBefore("\\bin"), "..\\Templates\\" + fileName);
+            
+            return File.ReadAllText(path);
+        }
+        private string LoadResourceInclude(string fileName)
+        {
+            var path = Path.Combine(Assembly.GetExecutingAssembly().Location.SubstringBefore("\\bin"), "..\\Templates\\Resources\\" + fileName);
+            
+            return File.ReadAllText(path);
+        }
+
+        private string LoadTemplate(string fileName)
+        {
+            var path = Path.Combine(Assembly.GetExecutingAssembly().Location.SubstringBefore("\\bin"), "..\\Tests\\" + fileName);
+            
+            return File.ReadAllText(path);
+        }
+
+        private string LoadTest(string fileName)
+        {
+            var path = Path.Combine(Assembly.GetExecutingAssembly().Location.SubstringBefore("\\bin"), "..\\Tests", fileName);
+            
+            return File.ReadAllText(path);
+        }
+    }
+}
